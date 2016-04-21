fd3451adf6d021a479dabfd38d2da65a27ef69d2
setheid <setheid@gmail.com>
2016-04-21 11:20:42 -0700

    initial commit

---
+++ SethMoore/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+let app = angular.module('app', []);
+
+app.directive('coffeeApp', () => {
+  return {
+    restrict: 'A',
+    controller: ($scope) => {
+      $scope.loggedin = false;
+      $scope.login = () => $scope.loggedin = !$scope.loggedin;
+      $scope.coffees = [];
+    }
+  }
+});
+
+app.directive('loginForm', () => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      login: '&',
+      loggedin: '='
+    },
+    templateUrl: 'templates/login.html',
+    controller: ($scope) => {
+
+    }
+  }
+});
+
+app.directive('overview', () => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      loggedin: '=',
+      coffees: '='
+    },
+    templateUrl: 'templates/overview.html',
+    controller: ($scope) => {
+
+    }
+  }
+});
+
+app.directive('notes', () => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      loggedin: '=',
+      coffees: '='
+    },
+    templateUrl: 'templates/notes.html',
+    controller: ($scope) => {
+      $scope.coffee = {};
+      $scope.coffee.stars = 0;
+      $scope.addReview = function(coffees, coffee) {
+        var newCoffee = coffee;
+        $scope.coffee = {};
+        $scope.coffee.stars = 0;
+        coffees.push(newCoffee);
+      }
+    }
+    // link: function($scope, element, attrs) {
+    //   element.find('i').bind("click", function(e){
+    //     var num = $(this).index() + 1;
+    //     $scope.$apply(function() {
+    //       $scope.coffee.stars = num;
+    //     });
+    //   });
+    // }
+  }
+});
+
+app.directive('stars', () => {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      coffee: '='
+    },
+    templateUrl: 'templates/stars.html',
+    link: function($scope, element, attrs) {
+      element.find('i').bind("click", function(e){
+        var num = $(this).index() + 1;
+        $scope.$apply(function() {
+          $scope.coffee.stars = num;
+        });
+      });
+    }
+  }
+});
